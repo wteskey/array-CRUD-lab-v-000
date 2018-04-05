d843f01e1016910e9914b8857dae072ac38ddd09
William J Teskey <willjteskey@gmail.com>
2018-04-05 19:49:52 +0000

    automatically backed up by learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -18,7 +18,7 @@ end
 
 def remove_element_from_end_of_array(array)
   days = ["Monday", "Tuesday", "Friday", "arrays!"]
-  "arrays!" = days.pop
+  ("arrays!") = days.pop
 end
 
 def remove_element_from_start_of_array(array)
