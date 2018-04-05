5e851ca0aab233a2e1d6b2d0906a29bcc3e6e70c
William J Teskey <willjteskey@gmail.com>
2018-04-05 19:43:13 +0000

    automatically backed up by learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -7,12 +7,13 @@ def create_an_array
 end
 
 def add_element_to_end_of_array(array, element)
-  countries = ["America", "Canada", "Germany"]
+  countries = ["America", "Canada", "Germany", "Congo"]
 countries << "Australia"
 end
 
 def add_element_to_start_of_array(array, element)
-  candies.unshift("nomnoms")
+  planets = ["Earth", "Neptune", "Mars", "Jupiter"]
+  planets.unshift("Pluto")
 end
 
 def remove_element_from_end_of_array(array)
