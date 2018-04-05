dab0fc256f1a778e07ce92f466b1daafa7a8417c
William J Teskey <willjteskey@gmail.com>
2018-04-05 19:52:22 +0000

    automatically backed up by learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -22,7 +22,7 @@ def remove_element_from_end_of_array(array)
 end
 
 def remove_element_from_start_of_array(array)
- expressions 
+ expressions = ["wow", "great", "super", "jeez"]
   wow = expressions.shift
 end
 
