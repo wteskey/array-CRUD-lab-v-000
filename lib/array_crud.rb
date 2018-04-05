9411d92f0a345d7134d91ee7ecf0dc8b7fe2c8c1
William J Teskey <willjteskey@gmail.com>
2018-04-05 19:44:00 +0000

    automatically backed up by learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -8,7 +8,7 @@ end
 
 def add_element_to_end_of_array(array, element)
   countries = ["America", "Canada", "Germany", "Congo"]
-countries << "Australia"
+countries << "arrays!"
 end
 
 def add_element_to_start_of_array(array, element)
